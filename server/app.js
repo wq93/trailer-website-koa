ecc63f951297a12e3d07d437bba72d16706d666a
wq93 <214180117@qq.com>
2018-08-21 11:12:38 +0800

    练习koa-session中间件

--- server/app.js
+++ server/app.js
@@ -1,11 +1,17 @@
 const Koa = require('koa')
 const app = new Koa()
 const logger = require('koa-logger')
-
+const session = require('koa-session')
+app.keys = ['Hi wq']
 app.use(logger())
+app.use(session(app))
 
 app.use(async (ctx, next) => {
-  ctx.body = 'hello word'
+  if (ctx.path === '/favicon.ico') return
+  let n = ctx.session.views || 0
+  ctx.session.views = ++n
+  ctx.body = n + 'views'
+
 })
 
 app.listen(2333, () => {
